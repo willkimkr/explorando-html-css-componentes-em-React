b90f7ffe55172684fede3e5771abe5ae74b91571
willkimkr <98241898+willkimkr@users.noreply.github.com>
2024-11-09 14:09:58 +0000

    Criacao do component '<Card/>'

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
   return (
     <>
-      <div className='card'>
-        <h1>Java</h1>
-        <img src="https://salvatore.academy/devmon/1_java.png" width="200" />
-      </div>
+      <Card />
+      <Card />
+
     </>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,7 @@
+export default function Card() {
+  return <div className='card'>
+    <h1>Java</h1>
+    <img src="https://salvatore.academy/devmon/1_java.png" width="200" />
+  </div>
+
+}
